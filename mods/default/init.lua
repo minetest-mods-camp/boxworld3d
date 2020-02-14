59aeebf28c01c7f3f8e52fef0b175de60a3887d7
Hume2 <teratux.mail@gmail.com>
2020-02-14 15:56:53 +0100

    add default

---
+++ mods/default/init.lua
@@ -0,0 +1,52 @@
+-- Minetest 0.4 mod: default
+-- See README.txt for licensing and other information.
+
+-- The API documentation in here was moved into game_api.txt
+
+-- Definitions made by this mod that other mods can use too
+default = {}
+
+default.LIGHT_MAX = 14
+
+-- GUI related stuff
+minetest.register_on_joinplayer(function(player)
+	player:set_formspec_prepend([[
+			bgcolor[#080808BB;true]
+			background[5,5;1,1;gui_formbg.png;true]
+			listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF] ]])
+end)
+
+function default.get_hotbar_bg(x,y)
+	local out = ""
+	for i=0,7,1 do
+		out = out .."image["..x+i..","..y..";1,1;gui_hb_bg.png]"
+	end
+	return out
+end
+
+default.gui_survival_form = "size[8,8.5]"..
+			"list[current_player;main;0,4.25;8,1;]"..
+			"list[current_player;main;0,5.5;8,3;8]"..
+			"list[current_player;craft;1.75,0.5;3,3;]"..
+			"list[current_player;craftpreview;5.75,1.5;1,1;]"..
+			"image[4.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
+			"listring[current_player;main]"..
+			"listring[current_player;craft]"..
+			default.get_hotbar_bg(0,4.25)
+
+minetest.register_item(":", {
+	type = "none",
+	wield_image = "wieldhand.png",
+	wield_scale = {x=1,y=1,z=2.5},
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level = 0,
+		groupcaps = {
+			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
+		},
+		damage_groups = {fleshy=1},
+	}
+})
+
